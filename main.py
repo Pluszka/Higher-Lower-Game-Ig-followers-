a1483fed06932f5d46a1bdb28b382477f1dc3b09
KarolinaKoza <97195524+Pluszka@users.noreply.github.com>
2022-01-17 21:49:51 +0000

    Compare function

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+#popraw losowanie tych samych osób
 from random import choice
 from game_data import data
 
 POSITION_A='Compare A: '
 POSITION_B='Agnist B: '
+
+def compare(person_a_score, person_b_score,player_type):
+  if (person_a_score > person_b_score and player_type=='A') or (person_a_score  <person_b_score and player_type=='B'):
+    return True
+  else:
+    return False
 
 def choose_person():
   return choice(data)
@@ -15,7 +22,16 @@ def descriptions(position):
   desc=person_infos['description']
   score=person_infos['follower_count']
   print(f'{position}{name}, {desc} from {country}')
+  return score
 
-print('Welcome to Higer or Lower Game. You must guess which one of those accounts have more followers on Instagram. Good luck!')
+def battle():
+  a_score=descriptions(POSITION_A)
+  b_score=descriptions(POSITION_B)
+  answer=input("Type 'A' for first account or 'B' for second one. ").upper()
+  print('Hint: ', a_score,b_score)
+  print(compare(a_score,b_score, answer))
 
-descriptions(POSITION_A)
+
+print('Welcome to Higer or Lower Game. You must guess which one of those accounts have more followers on Instagram.\nGood luck!')
+
+battle()
